0d27128441f29b31ccffa85a548262ee49424176
Jagadeesh Krishnanjanappa <jagadeesh@alifsemi.com>
2024-05-28 23:34:09 +0530

    sample: ipm_arm_mhuv2: Fix MHU transfer issue between HP and HE
    - The flags msg_sent and msg_received are NOT defined as volatile
    due to which the compiler optimizes the code and does not send
    pending MHU messages 19 times.
    
    In fact, msg_sent and msg_received are modified in callbacks
    which are called by ISR on receiving IRQs.
    
    - Remove redundant recv_count variable.
    
    Signed-off-by: Jagadeesh Krishnanjanappa <jagadeesh@alifsemi.com>

--- samples/drivers/ipm/ipm_arm_mhuv2/rtss_he/src/for_rtss_hp_mhu0.c
+++ samples/drivers/ipm/ipm_arm_mhuv2/rtss_he/src/for_rtss_hp_mhu0.c
@@ -15,17 +15,13 @@
 const struct device *mhu0_r;
 const struct device *mhu0_s;
 
-static bool msg_sent;
-static bool msg_received;
-uint8_t recv_count;
+static volatile bool msg_sent;
+static volatile bool msg_received;
 
 static void recv_cb(const struct device *mhuv2_ipmdev, uint32_t *user_data)
 {
 	printk("MSG received is 0x%x\n", *user_data);
-	if (recv_count > 0)
-		++recv_count;
-	else
-		msg_received = true;
+	msg_received = true;
 }
 
 static void send_cb(const struct device *mhuv2_ipmdev, uint32_t *user_data)
@@ -39,7 +35,6 @@ static void send(void)
 	uint32_t set_mhu = 0xADDEDACE;
 
 	msg_received = false;
-	recv_count = 0;
 	msg_sent = false;
 	mhuv2_ipm_send(mhu0_s, 0, &set_mhu);
 	while (!msg_sent)
