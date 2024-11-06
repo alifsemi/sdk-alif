456afa780e2d4cb0e7270020193121e3abf5329c
Sudhir Sreedharan <sudhir@alifsemi.com>
2024-11-06 10:21:22 +0530

    samples: drivers: pm: system_off: license update
    Added proprietary license.
    
    Signed-off-by: Sudhir Sreedharan <sudhir@alifsemi.com>

--- samples/drivers/pm/system_off/src/main.c
+++ samples/drivers/pm/system_off/src/main.c
@@ -1,6 +1,11 @@
-/*
- * Copyright (c) 2024 Alif Semiconductor
- * SPDX-License-Identifier: Apache-2.0
+/* Copyright (C) 2024 Alif Semiconductor - All Rights Reserved.
+ * Use, distribution and modification of this code is permitted under the
+ * terms stated in the Alif Semiconductor Software License Agreement
+ *
+ * You should have received a copy of the Alif Semiconductor Software
+ * License Agreement with this file. If not, please write to:
+ * contact@alifsemi.com, or visit: https: //alifsemi.com/license
+ *
  */
 
 #include <stdio.h>
