9497f2f76e72d2e090077c9ae24cb508ccac3e41
Marian Kindzer <maryan.kindzer@alifsemi.com>
2025-09-10 12:46:25 +0300

    drivers: i2s: i2s_sync: disables DMA when I2S is stopped
    Added fix to properly stop DMA on I2S stop.
    
    Signed-off-by: Marian Kindzer <maryan.kindzer@alifsemi.com>

--- drivers/i2s/i2s_sync/i2s_sync.c
+++ drivers/i2s/i2s_sync/i2s_sync.c
@@ -416,6 +416,15 @@ static void i2s_disable_tx(const struct device *dev)
 	struct i2s_sync_data *dev_data = dev->data;
 	struct i2s_t *i2s = dev_cfg->paddr;
 
+	if (dev_cfg->dma_tx.enabled) {
+		i2s_tx_dma_disable(i2s);
+		int ret = dma_stop(dev_cfg->dma_dev, dev_cfg->dma_tx.ch);
+
+		if (ret < 0) {
+			LOG_ERR("I2S:%s tx dma_stop failed %d\n", dev->name, ret);
+			return;
+		}
+	}
 	i2s_tx_channel_disable(i2s);
 	i2s_tx_block_disable(i2s);
 	i2s_tx_fifo_interrupt_disable(i2s);
@@ -436,6 +445,15 @@ static void i2s_disable_rx(const struct device *dev)
 	i2s_rx_overrun_interrupt_disable(i2s);
 	i2s_rx_fifo_clear(i2s);
 	channel_disable(&dev_data->rx);
+	if (dev_cfg->dma_rx.enabled) {
+		i2s_rx_dma_disable(i2s);
+		int ret = dma_stop(dev_cfg->dma_dev, dev_cfg->dma_rx.ch);
+
+		if (ret < 0) {
+			LOG_ERR("I2S:%s rx dma_stop failed %d\n", dev->name, ret);
+			return;
+		}
+	}
 }
 
 static int i2s_sync_disable_impl(const struct device *dev, enum i2s_dir dir)
