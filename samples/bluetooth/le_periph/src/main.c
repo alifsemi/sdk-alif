50f867ba7a2636126eea4dcefb43799fe06ddf8c
Juan Vazquez <juan.vazquez@alifsemi.com>
2025-11-10 13:58:04 +0200

    samples: bluetooth: le_periph: Add missing license.
    Add missing license to le_periph sample application.
    
    Signed-off-by: Juan Vazquez <juan.vazquez@alifsemi.com>

--- samples/bluetooth/le_periph/src/main.c
+++ samples/bluetooth/le_periph/src/main.c
@@ -1,3 +1,12 @@
+/* Copyright (C) 2025 Alif Semiconductor - All Rights Reserved.
+ * Use, distribution and modification of this code is permitted under the
+ * terms stated in the Alif Semiconductor Software License Agreement
+ *
+ * You should have received a copy of the Alif Semiconductor Software
+ * License Agreement with this file. If not, please write to:
+ * contact@alifsemi.com, or visit: https://alifsemi.com/license
+ */
+
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include "alif_ble.h"
