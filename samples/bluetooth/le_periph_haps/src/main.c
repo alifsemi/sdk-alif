c79ec438bb8a6cb55d67ddac79ddd870080b4240
Juha Heiskanen <juha.heiskanen@alifsemi.com>
2026-02-12 20:13:30 +0200

    samples: bluettoth: Updated Generic audio samples and address gen
    Integrated gaf advertisment and private identity definition.
    
    Enabled static random address gen for security samples with
    address storage.
    
    Signed-off-by: Juha Heiskanen <juha.heiskanen@alifsemi.com>

--- samples/bluetooth/le_periph_haps/src/main.c
+++ samples/bluetooth/le_periph_haps/src/main.c
@@ -27,13 +27,7 @@
 #include <alif/bluetooth/bt_scan_rsp.h>
 #include "gapm_api.h"
 #include "rwip_task.h"
-
-
-/* Define advertising address type */
-#define SAMPLE_ADDR_TYPE	ALIF_STATIC_RAND_ADDR
-
-/* Store and share advertising address type */
-static uint8_t adv_type;
+#include "ble_storage.h"
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
@@ -51,6 +45,11 @@ static struct app_env env = {
 	.con_lid = 0xFF
 };
 
+static struct connection_status app_con_info = {
+	.conidx = GAP_INVALID_CONIDX,
+	.addr.addr_type = 0xff,
+};
+
 /**
  * Bluetooth stack configuration
  */
@@ -59,7 +58,7 @@ static gapm_config_t gapm_cfg = {
 	.pairing_mode = GAPM_PAIRING_SEC_CON,
 	.privacy_cfg = GAPM_PRIV_CFG_PRIV_ADDR_BIT,
 	.renew_dur = 1500,
-	.private_identity.addr = {0xCA, 0xFE, 0xFA, 0xDE, 0x15, 0x08},
+	.private_identity.addr = {0},
 	.irk.key = {0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF1, 0x07, 0x08, 0x11, 0x22, 0x33, 0x44, 0x55,
 		    0x66, 0x77, 0x89},
 	.gap_start_hdl = 0,
@@ -77,9 +76,6 @@ static gapm_config_t gapm_cfg = {
 
 /* Load name from configuration file */
 #define DEVICE_NAME CONFIG_BLE_DEVICE_NAME
-
-/* Store advertising activity index for re-starting after disconnection */
-static uint8_t adv_actv_idx;
 
 /* HAPS callbacks */
 void app_hap_has_cb_bond_data(uint8_t con_lid, uint8_t cli_cfg_ntf_bf, uint8_t cli_cfg_ind_bf)
@@ -115,46 +111,16 @@ static hap_has_cb_t haps_cbs = {
 	.cb_set_preset_name_req = app_hap_has_cb_set_preset_name_req,
 };
 
-/* Advertisement functions */
-static uint16_t set_advertising_data(uint8_t actv_idx)
-{
-	uint16_t ret;
-	uint16_t comp_id = CONFIG_BLE_COMPANY_ID;
-
-	ret = bt_adv_data_set_manufacturer(comp_id, NULL, 0);
-
-	if (ret) {
-		LOG_ERR("AD manufacturer data fail %d", ret);
-		return ATT_ERR_INSUFF_RESOURCE;
-	}
-
-	ret = bt_adv_data_set_name_auto(DEVICE_NAME, strlen(DEVICE_NAME));
-
-	if (ret) {
-		LOG_ERR("AD device name data fail %d", ret);
-		return ATT_ERR_INSUFF_RESOURCE;
-	}
-
-	return bt_gapm_advertiment_data_set(actv_idx);
-}
-
 static uint16_t create_advertising(void)
 {
-	gapm_le_adv_create_param_t adv_create_params = {
-		.prop = GAPM_ADV_PROP_UNDIR_CONN_MASK,
-		.disc_mode = GAPM_ADV_MODE_GEN_DISC,
-		.tx_pwr = 0,
-		.filter_pol = GAPM_ADV_ALLOW_SCAN_ANY_CON_ANY,
-		.prim_cfg = {
-				.adv_intv_min = 160, /* 100 ms */
-				.adv_intv_max = 800, /* 500 ms */
-				.ch_map = ADV_ALL_CHNLS_EN,
-				.phy = GAPM_PHY_TYPE_LE_1M,
-			},
-	};
+	uint16_t err = bt_gaf_create_adv(DEVICE_NAME, strlen(DEVICE_NAME), &app_con_info.addr);
 
-	return bt_gapm_le_create_advertisement_service(adv_type, &adv_create_params, NULL,
-						      &adv_actv_idx);
+	if (err != GAF_ERR_NO_ERROR) {
+		LOG_ERR("Unable to configure GAF advertiser! Error %u (0x%02X)", err, err);
+		return err;
+	}
+	LOG_DBG("GAF advertiser is configured");
+	return err;
 }
 
 static uint16_t server_configure(void)
@@ -207,13 +173,13 @@ int main(void)
 
 	env.connected = false;
 
+	ble_storage_init();
+
 	/* Start up bluetooth host stack */
 	alif_ble_enable(NULL);
 
-	if (address_verification(SAMPLE_ADDR_TYPE, &adv_type, &gapm_cfg)) {
-		LOG_ERR("Address verification failed");
-		return -EADV;
-	}
+	/* Define Private identity */
+	bt_generate_private_identity(&gapm_cfg);
 
 	/* Configure Bluetooth Stack */
 	LOG_INF("Init gapm service");
@@ -235,19 +201,9 @@ int main(void)
 		return -1;
 	}
 
-	err = set_advertising_data(adv_actv_idx);
-	if (err) {
-		LOG_ERR("Advertisement data set fail %u", err);
-		return -1;
-	}
 
-	err = bt_gapm_scan_response_set(adv_actv_idx);
-	if (err) {
-		LOG_ERR("Scan response set fail %u", err);
-		return -1;
-	}
-
-	err = bt_gapm_advertisement_start(adv_actv_idx);
+	/* Start a Generic audio advertisement */
+	err = bt_gaf_adv_start(&app_con_info.addr);
 	if (err) {
 		LOG_ERR("Advertisement start fail %u", err);
 		return -1;
